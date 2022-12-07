9cf94c6fd9554392cb86f62e833eebc713914030
Vladimir Zarypov <krre31@gmail.com>
2022-12-07 06:42:14 +0200

    Convert StartScreen class to StatelessWidget

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class StartScreen extends StatefulWidget {
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
-  @override
-  State<StartScreen> createState() => _StartScreenState();
-}
-
-class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
