6d4bf6be311f264389c7afa2189f36e3ac6cfff3
Vladimir Zarypov <krre31@gmail.com>
2022-12-08 07:27:05 +0200

    Add widgets to input connection values

--- lib/screens/connect_database_screen.dart
+++ lib/screens/connect_database_screen.dart
@@ -9,12 +9,58 @@ class ConnectDatabaseScreen extends StatefulWidget {
 }
 
 class _ConnectDatabaseScreenState extends State<ConnectDatabaseScreen> {
+  String _ip = '';
+  String _port = '';
+  String _key = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
           AppBar(title: Text(AppLocalizations.of(context)!.connectDatabase)),
-      body: const Center(),
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                onChanged: (text) {
+                  _ip = text;
+                  setState(() {});
+                },
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.ip,
+                ),
+              ),
+              TextField(
+                onChanged: (text) {
+                  _port = text;
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.port,
+                ),
+              ),
+              TextField(
+                onChanged: (text) {
+                  _key = text;
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.key,
+                ),
+              ),
+              OutlinedButton(
+                  onPressed:
+                      _ip.isNotEmpty && _port.isNotEmpty && _key.isNotEmpty
+                          ? () {
+                              debugPrint(_ip);
+                            }
+                          : null,
+                  child: const Text("OK"))
+            ]),
+      ),
     );
   }
 }
