73c6d9c7f760bda4d3fbc43b27352de2452a8700
Vladimir Zarypov <krre31@gmail.com>
2022-12-09 06:44:11 +0200

    Wrap forms into Container widget

--- lib/screens/connect_database_screen.dart
+++ lib/screens/connect_database_screen.dart
@@ -19,33 +19,37 @@ class _ConnectDatabaseScreenState extends State<ConnectDatabaseScreen> {
       appBar:
           AppBar(title: Text(AppLocalizations.of(context)!.connectDatabase)),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                onChanged: (text) => _ip = text,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.ip,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  onChanged: (text) => _ip = text,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.ip,
+                  ),
                 ),
-              ),
-              TextField(
-                onChanged: (text) => _port = text,
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.port,
+                TextField(
+                  onChanged: (text) => _port = text,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.port,
+                  ),
                 ),
-              ),
-              TextField(
-                onChanged: (text) => _key = text,
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.key,
+                TextField(
+                  onChanged: (text) => _key = text,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.key,
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () => debugPrint('IP $_ip Port $_port Key $_key'),
-                  child: const Text("OK"))
-            ]),
+                ElevatedButton(
+                    onPressed: () =>
+                        debugPrint('IP $_ip Port $_port Key $_key'),
+                    child: const Text("OK"))
+              ]),
+        ),
       ),
     );
   }
--- lib/screens/create_database_screen.dart
+++ lib/screens/create_database_screen.dart
@@ -16,21 +16,24 @@ class _CreateDatabaseScreenState extends State<CreateDatabaseScreen> {
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.createDatabase)),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                onChanged: (text) => _name = text,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.name,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  onChanged: (text) => _name = text,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.name,
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () => debugPrint('Name $_name'),
-                  child: const Text("OK"))
-            ]),
+                ElevatedButton(
+                    onPressed: () => debugPrint('Name $_name'),
+                    child: const Text("OK"))
+              ]),
+        ),
       ),
     );
   }
