32b640fd5d351a556b5acbabf9866ae8c2bc1cee
Vladimir Zarypov <krre31@gmail.com>
2022-12-14 08:52:28 +0200

    Add dialog to enter note name

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_treeview/flutter_treeview.dart';
 
 class NotesScreen extends StatefulWidget {
   const NotesScreen({super.key});
@@ -10,12 +11,36 @@ class NotesScreen extends StatefulWidget {
   }
 }
 
+Future<void> showNameDialog(BuildContext context) async {
+  final l10n = AppLocalizations.of(context);
+
+  return await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(l10n!.name),
+      content: const TextField(
+        autofocus: true,
+      ),
+      actions: [ElevatedButton(onPressed: () {}, child: Text(l10n.ok))],
+    ),
+  );
+}
+
 class NotesScrenState extends State<NotesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.notes)),
-      body: const Center(),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.notes),
+        actions: [
+          IconButton(
+              onPressed: () => showNameDialog(context),
+              icon: const Icon(Icons.add_box_outlined))
+        ],
+      ),
+      body: TreeView(
+        controller: TreeViewController(),
+      ),
     );
   }
 }
