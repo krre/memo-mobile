4276c80411c9456822329fa9c8daf0e35b16e786
Vladimir Zarypov <krre31@gmail.com>
2022-12-16 04:34:01 +0200

    Move MemoApp class to memo_app.dart file

---
+++ lib/memo_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'screens/start_screen.dart';
+
+class MemoApp extends StatelessWidget {
+  const MemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memo',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('ru', ''),
+      ],
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:memo/main.dart';
+import 'package:memo/memo_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
