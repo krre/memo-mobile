a0125005dc3378ac6105ce69dd53cd73de1a6d8f
Vladimir Zarypov <krre31@gmail.com>
2022-12-24 12:47:56 +0200

    Add note editor

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -13,11 +13,17 @@ class NoteScreen extends StatefulWidget {
 
 class _NoteScreenState extends State<NoteScreen> {
   String title = '';
+  bool isEditable = false;
+  TextEditingController? _controller;
 
   @override
   void initState() {
     super.initState();
+
     _setTitle();
+    _setNote();
+
+    setState(() {});
   }
 
   _setTitle() async {
@@ -25,11 +31,50 @@ class _NoteScreenState extends State<NoteScreen> {
     setState(() {});
   }
 
+  _setNote() async {
+    final note = await Db.getInstance().getValue(widget.id, 'note');
+    setState(() {});
+    _controller = TextEditingController(text: note);
+  }
+
+  _updateNote() async {
+    await Db.getInstance().updateValue(widget.id, 'note', _controller!.text);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text(title),
+          actions: [
+            isEditable
+                ? IconButton(
+                    onPressed: () async {
+                      await _updateNote();
+                      isEditable = false;
+                      setState(() {});
+                    },
+                    icon: const Icon(Icons.save))
+                : IconButton(
+                    onPressed: () {
+                      isEditable = true;
+                      setState(() {});
+                    },
+                    icon: const Icon(Icons.edit))
+          ],
+        ),
+        body: isEditable
+            ? TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                controller: _controller,
+              )
+            : SelectableText(_controller != null ? _controller!.text : ''));
   }
 }
